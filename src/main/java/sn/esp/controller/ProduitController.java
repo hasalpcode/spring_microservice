f9c7f4c58a862bdabc8ae36d6ba3729701ddfb81
hasaslp <hasalpcode784@gmail.com>
2024-05-03 20:43:00 +0000

    add apiRest

--- src/main/java/sn/esp/controller/ProduitController.java
+++ src/main/java/sn/esp/controller/ProduitController.java
@@ -1,6 +1,7 @@
 package sn.esp.controller;
 
 import sn.esp.entity.*;
+import sn.esp.metier.ProduitProcessings;
 
 import java.util.List;
 
@@ -8,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 
 import sn.esp.dao.*;
 
@@ -23,6 +27,9 @@ import sn.esp.dao.*;
 public class ProduitController {
 	@Autowired
 	private ProduitDAO produitDao;
+	
+	@Autowired
+	private ProduitProcessings produitProcess;
 	
 	
 	@PostMapping("/produits")
@@ -39,6 +46,41 @@ public class ProduitController {
 		
 		
 		return new ResponseEntity(liste,HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/produits/{codeprod}")
+	public ResponseEntity<Produit> findProduit(@PathVariable("codeprod") Integer codeprod) {
+		
+		Produit produit  = produitDao.findProduit(codeprod);	
+		if(produit == null) {
+			throw new ResourceAccessException("ce produit n'existe pas! "+ codeprod);
+		}else{
+			return new ResponseEntity<Produit>(produit,HttpStatus.OK);
+		}
+		
+		
+		
+	}
+	
+	
+	@PutMapping("/produits/{codeprod}")
+	public ResponseEntity<Produit> editProduit(@PathVariable("codeprod") Integer codeprod, @RequestBody Produit produit) {
+		
+		Produit gettedProduit  = produitDao.findProduit(codeprod);	
+		if(gettedProduit == null) {
+			throw new ResourceAccessException("ce produit n'existe pas! "+ codeprod);
+		}else{
+			gettedProduit.setCodeprod(produit.getCodeprod());
+			gettedProduit.setNomprod(produit.getNomprod());
+			gettedProduit.setNomprod(produit.getNomprod());
+			gettedProduit.setPrix(produit.getPrix());
+			produitDao.editProduit(gettedProduit);
+			return new ResponseEntity<Produit>(produit,HttpStatus.OK);
+		}
+		
+		
+		
 	}
 
 }
